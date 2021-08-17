222776d3dc3d474901233f88dd2df6681bf5ba72
tomasitobambito <tomgacek0604@gmail.com>
2021-08-17 22:17:48 +0200

    Add project files.

---
+++ HmmmCheeseYum/ModEntry.cs
@@ -0,0 +1,12 @@
+﻿using StardewModdingAPI;
+
+namespace HmmmCheeseYum
+{
+	class ModEntry : StardewModdingAPI.Mod
+	{
+		public override void Entry(IModHelper helper)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
